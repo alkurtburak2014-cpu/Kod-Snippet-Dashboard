f4073e99ab4b86a568ce6e9beccb40866c90d328
Burak Alkurt <SENIN_EMAIL_ADRESIN>
2026-03-11 00:02:22 +0300

    Add stats, undo, sharing, autosave, PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'snippet-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
